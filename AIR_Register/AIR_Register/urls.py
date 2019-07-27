4d78eb1770570801745ae417004bb8d754f55214
KenGeng <gbvk123@163.com>
2019-07-27 19:56:39 +0800

    reformat

--- AIR_Register/AIR_Register/urls.py
+++ AIR_Register/AIR_Register/urls.py
@@ -1,4 +1,4 @@
-"""AIR_Resgister URL Configuration
+"""AIR_Register URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
--- AIR_Register/AIR_Register/wsgi.py
+++ AIR_Register/AIR_Register/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AIR_Resgister project.
+WSGI config for AIR_Register project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AIR_Resgister.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AIR_Register.settings')
 
 application = get_wsgi_application()
