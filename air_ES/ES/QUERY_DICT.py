dfbadd7c4e5931f0203639eeb7d204147388a157
yl_xps <ylizin@163.com>
2019-07-28 16:27:53 +0800

    fix

--- air_ES/ES/QUERY_DICT.py
+++ air_ES/ES/QUERY_DICT.py
@@ -1,28 +1,4 @@
-TEXT_QUERY={  
-    "query": 
-    {
-      "function_score": {
-        "functions": [
-          # {
-          #   "filter": {
-          #     "match": {
-          #       'title':'美国'  
-          #   }},
-          #   "weight": "4"
-          # },
-          # {
-          #   "filter": {
-          #     "match": {
-          #       "text": "cool"
-          #     }
-          #   },
-          #   "weight": "10"
-          # }
-        ],
-        "score_mode": "sum"
-      }
-}
-}
+
 
 def get_weighted_query(fields_texts_w):
     '''generate the QUERY for the field and text and weighted
@@ -30,7 +6,19 @@ def get_weighted_query(fields_texts_w):
     Arguments:
         fields_texts {[type]} -- [description]
     '''
+    TEXT_QUERY={  
+    "query": 
+    {
+      "function_score": {
+        "functions": [
+        ],
+        "score_mode": "sum"
+        }
+      }
+    }
+    
     func = TEXT_QUERY['query']['function_score']['functions']
+    
     for field,text,weighted in fields_texts_w:
         _f_t_w = {}
         _f_t_w['weight'] = str(weighted)
