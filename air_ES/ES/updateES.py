e1301d0bf1811e57624af9eabbf71b88dc827621
yl_xps <ylizin@163.com>
2019-07-28 16:07:01 +0800

    fix

--- air_ES/ES/updateES.py
+++ air_ES/ES/updateES.py
@@ -21,14 +21,14 @@ def update_ES_from_arxiv():
             # insert into ES
             bulk_q = generate_bulk_query(_newly_records,'arxiv')
             es.bulk(bulk_q)
-            _newly_records=[]
             #update the mongo mark
             _filter_query,_update_query = mark_added_query(_newly_ids)
             arxiv.update_many(_filter_query,_update_query)
+            _newly_records=[]
             _newly_ids = []    
             print('insert 2000 record.')        
     else:
-        bulk_q = generate_bulk_query(_newly_records)
+        bulk_q = generate_bulk_query(_newly_records,'arxiv')
         if bulk_q:
             es.bulk(bulk_q)
             _filter_query,_update_query = mark_added_query(_newly_ids)
