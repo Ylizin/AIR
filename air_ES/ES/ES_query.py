30a38848371865f4be22b463e995646c6939e558
yl_xps <ylizin@163.com>
2019-07-28 19:33:45 +0800

    add debug logging

--- air_ES/ES/ES_query.py
+++ air_ES/ES/ES_query.py
@@ -1,6 +1,9 @@
 from .ES_connector import get_es_conn
 from .QUERY_DICT import get_weighted_query
 import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
 
 def query_text(text:str , _index:str='test-index'):
     '''do query in es, the index param figures the index we gonna search
@@ -13,6 +16,7 @@ def query_text(text:str , _index:str='test-index'):
     '''
     #abstract and title have different weight in scoring
     _query = get_weighted_query([('abstract',text,4),('title',text,10)])
+    logging.debug(_query)
     if _index:
         try:
             res = get_es_conn().search(index=_index,body = _query,size=200)['hits']['hits'] # size is the num of returned docs
@@ -21,5 +25,6 @@ def query_text(text:str , _index:str='test-index'):
             return 'Inner ElasticSearch error.'
     else:
         raise Exception('Index should be None')
+    logging.debug([x['_score'] for x in res])
     return [x['_source'] for x in res]
     
