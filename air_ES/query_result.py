8649a7697520220965c3d58c84588b36463256da
KenGeng <gbvk123@163.com>
2019-08-07 13:22:13 +0800

    add annotation

--- air_ES/query_result.py
+++ air_ES/query_result.py
@@ -63,6 +63,10 @@ def get_acc_query_result(user_info,rough_info,index):
 
 
 def get_paper_info(f_ids,index = 'arxiv'):
+    '''
+        get paper/news/github info from ids
+        return a list
+    '''
     collection = get_collection(index)
     _q = get_record_info_query(f_ids)
     return list(collection.find(_q,projection={'updated':False}))
